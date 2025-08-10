14849300c19f9b27e90ef2ceb04db66902f4a5a0
MD SOHEB ANSARI <mdsohebansari234@gmail.com>
2025-08-10 18:06:24 +0530

    Time: 2 ms (6.39%), Space: 42.1 MB (78.7%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int x = 0;
+        for(int i = m; i < (m+n); i++){
+            nums1[i] = nums2[x];
+            x++;
+        }
+        int a = m+n;
+        for(int i = 0; i <= a-1; i++){
+            for(int j = 0; j < a-i-1; j++){
+                if(nums1[j] > nums1[j+1]){
+                    int temp = nums1[j];
+                    nums1[j] = nums1[j+1];
+                    nums1[j+1] = temp;
+                }
+            }
+        }
+    }
+}
