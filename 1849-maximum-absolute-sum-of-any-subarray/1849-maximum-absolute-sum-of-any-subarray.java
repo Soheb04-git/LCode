47f467e64547aa8ac9b7972b2b86e2c4c1df9519
MD SOHEB ANSARI <mdsohebansari234@gmail.com>
2025-08-19 02:37:38 +0530

    Time: 4 ms (81.18%), Space: 56.4 MB (76.82%) - LeetHub

---
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int n = nums.length;
+        int high = Integer.MIN_VALUE;
+        int low = Integer.MAX_VALUE;
+        int maxsum = 0;
+        int minsum = 0;
+        for(int i = 0; i < n; i++){
+            maxsum += nums[i];
+            if(maxsum < 0){
+                maxsum = 0;
+            }
+            minsum += nums[i];
+            if(minsum > 0){
+                minsum = 0;
+            }
+            high = Math.max(maxsum,high);
+            low = Math.min(minsum,low);
+        }
+        return Math.max(Math.abs(high),Math.abs(low));
+    }
+}
