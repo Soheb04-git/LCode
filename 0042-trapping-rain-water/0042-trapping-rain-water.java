e0d0e22d9d9420590022bf4c58aaa3494dd0545e
MD SOHEB ANSARI <mdsohebansari234@gmail.com>
2025-08-20 02:27:29 +0530

    Time: 1 ms (61.78%), Space: 46.7 MB (28.4%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int w,x,wt = 0, flagA = 1,flagB = 1;
+        /*w = hight of water
+          x = height of baar
+          wt = calculation of water*/
+        left[0] = height[0];
+        right[n-1] = height[n-1];
+        int j = n-2;
+        for(int i = 1; i < n; i++){
+            left[i] = Math.max(height[i],left[i-1]);
+            if(left[i] == height[i]) flagA++;
+            right[j] = Math.max(height[j],right[j+1]);
+            if(left[j] == height[j]) flagB++;
+            j--;
+        }
+        for(int i = 0; i < n; i++){
+            w = Math.min(left[i],right[i]);
+            x = height[i];
+            wt += (w-x);
+        }
+        if(n < 2 || flagA == n || flagB == n){
+            return 0;
+        }
+        return wt;
+    }
+}
