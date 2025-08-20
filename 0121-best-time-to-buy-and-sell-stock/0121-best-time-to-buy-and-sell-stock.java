c151c54534a51ab2ddb1c18b0ae4dbe7a2547f7a
MD SOHEB ANSARI <mdsohebansari234@gmail.com>
2025-08-20 21:02:48 +0530

    Time: 3 ms (5.49%), Space: 62.2 MB (12.07%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -2,10 +2,11 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int profit = 0;
-        int BP = prices[0];
+        int BP = prices[0],SP;
         for(int i = 0; i < n; i++){
             BP = Math.min(BP,prices[i]);
-            profit = Math.max(profit,prices[i]-BP);
+            SP = prices[i];
+            profit = Math.max(profit,SP-BP);
         }
         return profit;
     }
